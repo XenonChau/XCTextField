a8b4b75b32ec2d326e22110c5c810caf6bb11cdb
Xenon Chau <xenonchau@gmail.com>
2017-05-14 14:46:36 +0800

    add luhn algorithm, add cell phone check

--- XCTextFieldDemo/XCTextFieldDemo/XCTextField/UITextField+XCTextField.m
+++ XCTextFieldDemo/XCTextFieldDemo/XCTextField/UITextField+XCTextField.m
@@ -15,7 +15,7 @@
 /// @brief To save the origin layer.borderWidth;
 @property (readwrite, nonatomic) CGFloat borderWidth;
 /// @brief To save the origin layer.cornerRadius;
-@property (readwrite, nonatomic) CGFloat cornerRadius;
+@property (readwrite, nonatomic) CGFloat corprenerRadius;
 /// @brief When correct is true, border color will not change again.
 @property (readwrite, nonatomic) BOOL correct;
 @end
@@ -124,10 +124,17 @@
             [self IDCardCheck];
             break;
         }
-        case XCTextFieldTypePassword: {
-            [self passwordCheck];
+        case XCTextFieldTypeCellphone: {
+            [self cellphoneNumberCheck];
             break;
         }
+        case XCTextFieldTypeCreditCard: {
+            [self creditCardCheck];
+            break;
+        }
+        case XCTextFieldTypePassword:
+        case XCTextFieldTypeCAPTCHA:
+        case XCTextFieldTypeCurrency:
         default: {
             self.correct ? : [self correctAnimation];
             break;
@@ -262,6 +269,72 @@
 }
 
 #pragma mark - Text Check
+- (void)cellphoneNumberCheck {
+    BOOL result = NO;
+    /**
+     * 手机号码
+     * 移动：134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188
+     * 联通：130,131,132,152,155,156,185,186
+     * 电信：133,1349,153,180,189
+     * 虚拟运营商：170,177
+     */
+    NSString * MOBILE = @"^1(3[0-9]|5[0-35-9]|8[025-9]|7[07])\\d{8}$";
+    /**
+     10         * 中国移动：China Mobile
+     11         * 134[0-8],135,136,137,138,139,150,151,157,158,159,182,183，187,188
+     12         */
+    NSString * CM = @"^1(34[0-8]|(3[5-9]|5[017-9]|8[2378])\\d)\\d{7}$";
+    /**
+     15         * 中国联通：China Unicom
+     16         * 130,131,132,152,155,156,185,186
+     17         */
+    NSString * CU = @"^1(3[0-2]|5[256]|8[56])\\d{8}$";
+    /**
+     20         * 中国电信：China Telecom
+     21         * 133,1349,153,180,189
+     22         */
+    NSString * CT = @"^1((33|53|8[09])[0-9]|349)\\d{7}$";
+    /**
+     25         * 大陆地区固话及小灵通
+     26         * 区号：010,020,021,022,023,024,025,027,028,029
+     27         * 号码：七位或八位
+     28         */
+    // NSString * PHS = @"^0(10|2[0-5789]|\\d{3})\\d{7,8}$";
+    /**
+     31         * 虚拟运营商：腾讯大网卡、蚂蚁宝卡、B站小电视卡……
+     32         * 170...[176:CU, 177:CT, 178: CM]
+     @"^1(70|7[6-8])\\d{8}$"
+     33         */
+    NSString * VNO = @"^1(7[0-9])\\d{8}$";
+    
+    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
+    NSPredicate *regextestcm = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CM];
+    NSPredicate *regextestcu = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CU];
+    NSPredicate *regextestct = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT];
+    NSPredicate *regextestvno = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", VNO];
+    
+    if (self.text.length != 11) {
+        result = NO;
+    }
+    
+    if (([regextestmobile evaluateWithObject:self.text])
+        || ([regextestcm evaluateWithObject:self.text])
+        || ([regextestct evaluateWithObject:self.text])
+        || ([regextestcu evaluateWithObject:self.text])
+        || ([regextestvno evaluateWithObject:self.text])) {
+        result = YES;
+    } else {
+        result = NO;
+    }
+    
+    if (!result) {
+        self.checkResult = @"Phone number in invalid format.";
+        [self incorrectAnimation];
+    } else {
+        self.correct ? : [self correctAnimation];
+    }
+}
+
 - (void)emailCheck {
     NSString *emailRegEx = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
     NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegEx];
@@ -281,6 +354,95 @@
     /** Luhn algorithm
         @see https://en.wikipedia.org/wiki/Luhn_algorithm
      */
+    //取出最后一个字符
+    NSString *lastCharacter = [self.text substringFromIndex:[self.text length] - 1];
+    //取出前15/18个字符
+    NSString *preStr = [self.text substringToIndex:[self.text length] - 1];
+    //将前15/18个字符反序加进数组
+    NSMutableArray *reverseArr = [NSMutableArray array];
+    for (NSUInteger index = [preStr length]; index > 0; index--) {
+        [reverseArr addObject:[preStr substringWithRange:NSMakeRange(index - 1, 1)]];
+    }
+    //奇数位*2的乘积 < 9
+    NSMutableArray *evenNumberLessThanNineArray = [NSMutableArray array];
+    //奇数位*2的乘积 > 9
+    NSMutableArray *evenNumberGreaterThanNineArray = [NSMutableArray array];
+    
+    //偶数位数组
+    NSMutableArray *oddNumberArray = [NSMutableArray array];
+    
+    for (NSUInteger index = 0; index < [reverseArr count]; index++) {
+        if ((index + 1) % 2 == 1) {
+            //奇数位
+            NSUInteger evenNum = [[reverseArr objectAtIndex:index] integerValue];
+            if (evenNum * 2 < 9) {
+                [evenNumberLessThanNineArray addObject:[NSString stringWithFormat:@"%ld",(long)(evenNum * 2)]];
+            } else {
+                [evenNumberGreaterThanNineArray addObject:[NSString stringWithFormat:@"%ld",(long)(evenNum * 2)]];
+            }
+        } else {
+            //偶数位
+            [oddNumberArray addObject:[reverseArr objectAtIndex:index]];
+        }
+    }
+    
+    // ones-place number in evenNumberGreaterThanNineArray
+    NSMutableArray *onesPlaceNumArray = [NSMutableArray array];
+    
+    // tens-place number in evenNumberGreaterThanNineArray
+    NSMutableArray *tensPlaceNumArray = [NSMutableArray array];
+    
+    for (NSUInteger index = 0; index < [evenNumberGreaterThanNineArray count]; index++) {
+        @autoreleasepool {
+            NSString *onesPlaceStr = [NSString stringWithFormat:@"%ld",(long)[[evenNumberGreaterThanNineArray objectAtIndex:index] integerValue] % 10];
+            NSString *tensPlaceStr = [NSString stringWithFormat:@"%ld",(long)[[evenNumberGreaterThanNineArray objectAtIndex:index] integerValue] / 10];
+            [onesPlaceNumArray addObject:onesPlaceStr];
+            [tensPlaceNumArray addObject:tensPlaceStr];
+        }
+    }
+    
+    NSUInteger sumEvenNum = 0;
+    NSUInteger sumOddNum = 0;
+    NSUInteger sumOnesPlaceNum = 0;
+    NSUInteger sumTensPlaceNum = 0;
+    NSUInteger totalAmount = 0;
+    
+    for (NSUInteger index = 0; index < [evenNumberLessThanNineArray count]; index++) {
+        //奇数位*2 < 9数组之和
+        sumEvenNum += [[evenNumberLessThanNineArray objectAtIndex:index] integerValue];
+    }
+    
+    for (NSUInteger index = 0; index < [oddNumberArray count]; index++) {
+        //偶数位之和
+        sumOddNum += [[oddNumberArray objectAtIndex:index] integerValue];
+    }
+    
+    for (NSUInteger index = 0; index < [onesPlaceNumArray count]; index++) {
+        //奇数位*2 > 9数组 个位数之和
+        sumOnesPlaceNum += [[onesPlaceNumArray objectAtIndex:index] integerValue];
+    }
+    
+    for (NSUInteger index = 0; index < [tensPlaceNumArray count]; index++) {
+        //奇数位*2 > 9数组 十位数之和
+        sumTensPlaceNum += [[tensPlaceNumArray objectAtIndex:index] integerValue];
+    }
+    
+    //计算总和
+    totalAmount = sumEvenNum + sumOddNum + sumOnesPlaceNum + sumTensPlaceNum;
+    
+    //计算Luhm值
+    NSUInteger luhm = totalAmount % 10 == 0 ? 10 : totalAmount % 10;
+    
+    NSUInteger result = 10 - luhm;
+    
+    if ([lastCharacter integerValue] != result) {
+        self.checkResult = @"Credit card number in invalid format.";
+        [self incorrectAnimation];
+    } else {
+        self.correct ? : [self correctAnimation];
+    }
+    
+    
 }
 
 - (void)IDCardCheck {
